cccdd374f2b6791ed4107ca5c5293fbca2787433
Ruslan <ruslan56781@mail.ru>
2022-10-30 20:54:55 +0300

    refactoring

--- example/server/main.cpp
+++ example/server/main.cpp
@@ -1,74 +1,70 @@
+#include <Exception.hpp>
 #include <iostream>
 #include <tcpServer.hpp>
-#include <udpServer.hpp>
 #include <template.cpp>
-#include <Exception.hpp>
+#include <udpServer.hpp>
 
-constexpr size_t buf_size=256;
+constexpr size_t buf_size = 256;
 
 using namespace wrapper;
 
-class Server{
+class Server {
 public:
-    Server(const char *addr_IP,uint16_t _port,domain_socket _type=domain_socket::IPv4)
-    {
-      serv=MakeSocketPtr<tcpServer>(addr_IP,_port,_type);
+  Server(const char *addr_IP, uint16_t _port,
+         domain_socket _type = domain_socket::IPv4) {
+    serv = MakeSocketPtr<tcpServer>(addr_IP, _port, _type);
 
-      serv->Bind();
+    serv->Bind();
 
-      serv->Listen();
+    serv->Listen();
+  }
+
+public:
+  void MessageHandle() {
+
+    char *_buffer = new char[buf_size]();
+
+    auto client = serv->Accept();
+
+    if (client < 0) {
+      throw socket_error("Faield to accept client !");
+    } else {
+      std::cout << "Client has been accepted !" << std::endl;
     }
 
-public:
-    void MessageHandle(){
+    while (client) {
 
-     char *_buffer=new char[buf_size]();
+      auto resv_bytes = serv->Recv(client, _buffer, buf_size);
 
-      auto client = serv->Accept();
-
-      if(client<0){
-           throw socket_error("Faield to accept client !");
-      }else{
-          std::cout<<"Client has been accepted !"<<std::endl;
+      if (resv_bytes <= 0) {
+        std::cerr << "Failed to recevice data from socket !";
       }
 
-      while(client){
-
-          auto resv_bytes=serv->Recv(client,_buffer,buf_size);
-
-          if(resv_bytes<=0){
-              std::cerr<<"Failed to recevice data from socket !";
-          }
-
-          if(*_buffer=='Q'){
-              std::cout<<"Client disconnected !"<<std::endl;
-              exit(1);
-          }
-
-          std::cout<<"Message from client : "<<_buffer;
-
-          memset(_buffer,0,buf_size);
+      if (*_buffer == 'Q') {
+        std::cout << "Client disconnected !" << std::endl;
+        exit(1);
       }
-        delete[] _buffer;
+
+      std::cout << "Message from client : " << _buffer;
+
+      memset(_buffer, 0, buf_size);
     }
+    delete[] _buffer;
+  }
 
-    ~Server() = default ;
+  ~Server() = default;
 
 private:
-    sock_template<tcpServer> serv;
-
+  sock_template<tcpServer> serv;
 };
 
+int main() {
+  try {
+    Server s("127.0.0.1", 12345);
+    s.MessageHandle();
+  } catch (const socket_error &_error) {
+    std::cerr << _error.what() << std::endl;
+  }
 
-int main()
-{
-    try{
-      Server s("127.0.0.1",12345);
-         s.MessageHandle();
-    }
-    catch(const socket_error &_error){
-        std::cerr<<_error.what()<<std::endl;
-    }
-
-    return 0;
+  return 0;
 }
