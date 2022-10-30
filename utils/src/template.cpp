320b0c2097a25b5f5dceff7aaf086fb6c0a1a232
Ruslan <ruslan56781@mail.ru>
2022-10-31 00:29:42 +0300

    change constructor

--- utils/src/template.cpp
+++ utils/src/template.cpp
@@ -4,9 +4,9 @@
 namespace wrapper
 {
 
-        template<class _Type>
-        sock_template<_Type>::sock_template(const char *addr_IP, uint16_t _port, domain_socket _type)
-            :sock_ptr(new _Type(addr_IP,_port,_type)) {     }
+    //    template<class _Type>
+        //sock_template<_Type>::sock_template(const char *addr_IP, uint16_t _port, domain_socket _type)
+         //   :sock_ptr(new _Type(addr_IP,_port,_type)) {     }
 
         template<class _Type>
         sock_template<_Type>::sock_template(_Type *_sock) noexcept
